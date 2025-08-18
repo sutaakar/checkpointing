03d406bc6e751463e423be11a44ab61954d936fe
Karel Suta <ksuta@redhat.com>
2025-08-18 11:23:47 +0200

    Move widget

---
+++ kserve_deployer/__init__.py
@@ -0,0 +1,9 @@
+"""KServe Deployer Widget Package
+
+A Jupyter widget for deploying KServe InferenceServices from notebooks.
+"""
+
+from .widget import KServeDeployer
+
+__version__ = "0.1.0"
+__all__ = ["KServeDeployer"]
